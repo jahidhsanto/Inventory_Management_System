78cc21a8ac8a798c9ddb004062541e98577bfb1d
Md. Jahid Hassan <jahidhsanto@gmail.com>
2025-03-28 22:32:57 +0600

    User Logout DONE

---
+++ STORE_FINAL/UserDashboard/Logout.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/UserDashboard/MasterDashboard.Master" AutoEventWireup="true" CodeBehind="Logout.aspx.cs" Inherits="STORE_FINAL.UserDashboard.Logout" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ STORE_FINAL/UserDashboard/Logout.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace STORE_FINAL.UserDashboard
+{
+	public partial class Logout : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+            // Clear the session and any authentication cookies
+            Session.Abandon();  // End the session
+            Session.Clear();    // Clear session data
+
+            // Optionally, clear any authentication cookies if needed (if using forms authentication)
+            if (Request.Cookies[".ASPXAUTH"] != null)
+            {
+                var authCookie = new HttpCookie(".ASPXAUTH");
+                authCookie.Expires = DateTime.Now.AddDays(-1); // Expire the cookie
+                Response.Cookies.Add(authCookie); // Add the expired cookie
+            }
+
+            Response.Redirect("~");
+        }
+    }
+}
